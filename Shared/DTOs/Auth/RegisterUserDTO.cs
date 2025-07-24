083f27ee235b0aec7924585e73c897907897e50c
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-24 17:12:31 +0800

    Update RegisterUserDTO.cs

--- Shared/DTOs/Auth/RegisterUserDTO.cs
+++ Shared/DTOs/Auth/RegisterUserDTO.cs
@@ -14,7 +14,7 @@ using Shared.Enums;
             public string Gender { get; set; } = string.Empty;
             public string Email { get; set; } = string.Empty;
             public string PasswordHash { get; set; } = string.Empty;
-        public UserRole Role { get; set; } = UserRole.User;
-        public byte[]? ProfilePicture { get; set; }
+            public UserRole Role { get; set; } = UserRole.User;
+       
         }
     }
