198dd46d457afe7b922c5381c3a7a3f30f82159f
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-24 17:11:47 +0800

    Update UserDTO.cs

--- Shared/DTOs/UserDTO.cs
+++ Shared/DTOs/UserDTO.cs
@@ -10,9 +10,6 @@ namespace Shared.DTOs
         public string MiddleName { get; set; } = string.Empty;
         public string Gender { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-
         public UserRole Role { get; set; } = UserRole.User;
-
-        public byte[]? ProfilePicture { get; set; }
     }
 }
