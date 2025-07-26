80aaafe38d7462d881408f71f8013871a0ac39cb
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-26 21:01:47 +0800

    Update IDeviceService.cs

--- Server/Services/Interfaces/IDeviceService.cs
+++ Server/Services/Interfaces/IDeviceService.cs
@@ -6,7 +6,6 @@ namespace projServer.Services.Interfaces
     {
         Task AddDeviceAsync(DeviceDTO deviceDto);
         Task<List<DeviceDTO>> GetAllDeviceAsync();
-  
         Task<bool> UpdateDeviceAsync(DeviceDTO deviceDto);
         Task DeleteDevice(int id);
     }
