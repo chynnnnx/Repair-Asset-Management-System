5e8c7c41ece03460102db0ccf0ba5816a113588c
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-27 22:16:13 +0800

    Add files via upload

---
+++ Server/Controllers/RoomController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using projServer.Services.Interfaces;
+using Shared.DTOs;
+
+namespace projServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> AddRoom([FromBody] RoomDTO roomDto)
+        {
+            if (roomDto == null)
+                return BadRequest();
+
+            await _roomService.AddRoomAsync(roomDto);
+            return Ok(new { message = "Room added successfully!" });
+        }
+
+        [Authorize(Roles = "User,Admin")]
+        [HttpGet]
+        public async Task<ActionResult<List<RoomDTO>>> GetAllRooms()
+        {
+            var rooms = await _roomService.GetAllRoomsAsync();
+            return Ok(rooms);
+        }
+
+        [Authorize(Roles = "User,Admin")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RoomDTO>> GetRoomById(int id)
+        {
+            var room = await _roomService.GetRoomByIdAsync(id);
+            if (room == null)
+                return NotFound();
+            return Ok(room);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateRoom([FromBody] RoomDTO roomDto)
+        {
+            if (roomDto == null)
+                return BadRequest();
+
+            await _roomService.UpdateRoomAsync(roomDto);
+            return Ok(new { message = "Room updated successfully!" });
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRoom(int id)
+        {
+            await _roomService.DeleteRoomAsync(id);
+            return Ok(new { message = "Room deleted successfully!" });
+        }
+    }
+}
