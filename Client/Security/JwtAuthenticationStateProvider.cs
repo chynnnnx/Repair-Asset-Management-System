606b46567a9d75335043cd6dec427bfacc9f5041
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-28 21:35:22 +0800

    Add files via upload

---
+++ Client/Security/JwtAuthenticationStateProvider.cs
@@ -0,0 +1,98 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+
+namespace Client.Security
+{
+    public class JwtAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+      
+
+        public JwtAuthenticationStateProvider(
+            ILocalStorageService localStorage,
+            HttpClient http
+           ) 
+        {
+            _localStorage = localStorage;
+            _http = http;
+           
+        }
+
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+
+            try
+            {
+                var token = await _localStorage.GetItemAsync<string>("authToken");
+
+                if (!string.IsNullOrWhiteSpace(token))
+                {
+                    var claims = ParseClaimsFromJwt(token);
+
+                   
+                    var expClaim = claims.FirstOrDefault(c => c.Type == "exp")?.Value;
+                    if (expClaim != null && long.TryParse(expClaim, out long exp))
+                    {
+                        var expTime = DateTimeOffset.FromUnixTimeSeconds(exp);
+                        if (expTime < DateTimeOffset.UtcNow)
+                        {
+                         
+                            await _localStorage.RemoveItemAsync("authToken");
+                            NotifyUserLogout();
+                            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+                        }
+                    }
+
+                    identity = new ClaimsIdentity(claims, "jwt");
+                    _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                    user = new ClaimsPrincipal(identity);
+                }
+            }
+            catch
+            {
+              
+            }
+
+            return new AuthenticationState(user);
+        }
+
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+        }
+
+        public void NotifyUserLogout()
+        {
+            var anonymousUser = new ClaimsPrincipal(new ClaimsIdentity());
+            var state = Task.FromResult(new AuthenticationState(anonymousUser));
+            NotifyAuthenticationStateChanged(state);
+        }
+
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value?.ToString() ?? ""));
+        }
+
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            base64 = base64.PadRight(base64.Length + (4 - base64.Length % 4) % 4, '=');
+            return Convert.FromBase64String(base64);
+        }
+    }
+}
