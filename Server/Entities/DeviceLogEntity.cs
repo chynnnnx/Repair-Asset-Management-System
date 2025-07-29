08a46a4ab24a8575f88c9a69c220b79be850dfd4
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-29 19:02:51 +0800

    Update DeviceLogEntity.cs

--- Server/Entities/DeviceLogEntity.cs
+++ Server/Entities/DeviceLogEntity.cs
@@ -7,10 +7,8 @@ namespace projServer.Entities
     {
         [Key]
         public int LogId { get; set; }
-
         public int? DeviceID { get; set; } 
         public DeviceEntity? Device { get; set; }
-
         public string? Note { get; set; }
         public int? ActionById { get; set; }
         public UserEntity? ActionBy { get; set; }
