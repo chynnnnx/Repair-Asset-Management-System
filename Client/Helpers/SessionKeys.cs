7eb97ced666e1714bd5456fa00f6096e4978c8ad
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-07-30 20:13:36 +0800

    Update SessionKeys.cs

--- Client/Helpers/SessionKeys.cs
+++ Client/Helpers/SessionKeys.cs
@@ -5,7 +5,6 @@
         public const string AuthToken = "authToken";
         public const string SessionUserId = "sessionUserId";
         public const string Tag = "tag";
-        public const string IsPCLogin = "isPCLogin";
         public const string SessionEmail = "sessionEmail";
         public const string SessionRole = "sessionRole";
         public const string RoomId = "roomId";
