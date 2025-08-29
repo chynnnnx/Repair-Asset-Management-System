c109f508bdd1ccc60b1d3d3aa30a689bc214416e
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-08-29 22:08:30 +0800

    Add files via upload

---
+++ Client/ViewModels/RoomViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Shared.DTOs;
+
+namespace Client.ViewModels
+{
+    
+    public class RoomViewModel
+    {
+        public int RoomId { get; set; }
+        public string RoomName { get; set; } = string.Empty;
+    }
+
+    public static class RoomMappings
+    {
+        public static RoomViewModel ToViewModel(this RoomDTO dto) =>
+            new RoomViewModel
+            {
+                RoomId = dto.RoomId,
+                RoomName = dto.RoomName
+            };
+
+        public static RoomDTO ToDTO(this RoomViewModel vm) =>
+            new RoomDTO
+            {
+                RoomId = vm.RoomId,
+                RoomName = vm.RoomName
+            };
+    }
+
+}
