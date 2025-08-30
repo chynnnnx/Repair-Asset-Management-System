12869d95d0aee77ada335761eb11fd91a0b7ea9d
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-08-30 20:27:34 +0800

    Update IDeviceService.cs

--- Client/Services/Interfaces/IDeviceService.cs
+++ Client/Services/Interfaces/IDeviceService.cs
@@ -1,12 +1,12 @@
-﻿using Shared.DTOs;
+using Client.ViewModels;
 
 namespace Client.Services.Interfaces
 {
     public interface IDeviceService
     {
-        Task<List<DeviceDTO>> GetAllDevicesAsync();
-        Task<bool> AddDeviceAsync(DeviceDTO deviceDto);
-        Task<bool> UpdateDeviceAsync(DeviceDTO deviceDto);
-        Task<bool> DeleteDeviceAsync(int deviceId);
+        Task<List<DeviceViewModel>> GetAllDevicesAsync();
+        Task<bool> AddDeviceAsync(DeviceViewModel device);
+        Task<bool> UpdateDeviceAsync(DeviceViewModel device);
+            Task<bool> DeleteDeviceAsync(int deviceId);
     }
 }
