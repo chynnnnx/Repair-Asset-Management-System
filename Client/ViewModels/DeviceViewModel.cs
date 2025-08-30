a57aae1a677be72e779c352efa1089d550ea0520
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-08-30 20:43:24 +0800

    Add files via upload

---
+++ Client/ViewModels/DeviceViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Shared.DTOs;
+using Shared.Enums;
+
+
+namespace Client.ViewModels
+{
+    public class DeviceViewModel
+    {
+        public int DeviceID { get; set; }
+        public string Tag { get; set; } = string.Empty;
+        public DeviceStatus Status { get; set; } = DeviceStatus.Offline;
+        public int RoomId { get; set; }
+        public string RoomName { get; set; } = "N/A";
+    }
+
+   public static class  DeviceMappings
+    {
+        public static DeviceViewModel ToViewModel(this DeviceDTO dto) =>
+            new DeviceViewModel
+            {
+                DeviceID = dto.DeviceID,
+                Tag = dto.Tag,
+                Status = dto.Status,
+                RoomId = dto.RoomId,
+                RoomName = dto.RoomName
+            };
+
+        public static DeviceDTO ToDTO(this DeviceViewModel vm) =>
+            new DeviceDTO
+            {
+                DeviceID = vm.DeviceID,
+                Tag = vm.Tag,
+                Status = vm.Status,
+                RoomId = vm.RoomId,
+                RoomName = vm.RoomName
+            };
+    }
+}
+
+
---
+++ Client/ViewModels/UserViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Shared.DTOs;
+using Shared.Enums;
+
+namespace Client.ViewModels
+{
+    public class UserViewModel
+    {
+        public int UserID { get; set; }
+        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public UserRole Role { get; set; } = UserRole.User;
+    }
+
+    public static class UserMappings
+    {
+        public static UserViewModel ToViewModel(this UserDTO dto) =>
+            new UserViewModel
+            {
+                UserID = dto.UserID,
+                LastName = dto.LastName,
+                FirstName = dto.FirstName,
+                MiddleName = dto.MiddleName,
+                Gender = dto.Gender,
+                Email = dto.Email,
+                Role = dto.Role
+
+            };
+        public static UserDTO ToDTO(this UserViewModel vm) =>
+            new UserDTO
+            {
+                UserID = vm.UserID,
+                LastName = vm.LastName,
+                FirstName = vm.FirstName,
+                MiddleName = vm.MiddleName,
+                Gender = vm.Gender,
+                Email = vm.Email,
+                Role = vm.Role
+            };
+    }
+}
