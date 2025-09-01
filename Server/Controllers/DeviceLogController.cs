ef8990d41cfc556840dac5131276c15fd5d7cd47
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-09-01 19:59:33 +0800

    added xml docs to devicelogcontroller

--- Server/Controllers/DeviceLogController.cs
+++ Server/Controllers/DeviceLogController.cs
@@ -7,7 +7,12 @@ using AutoMapper;
 using System.Formats.Asn1;
 
 namespace projServer.Controllers
-{
+{  
+    /// <summary>
+    /// handles all operations related to device logs such as
+    /// retrieving logs by device id or getting all logs.
+    /// </summary>
+    
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(Roles = "Admin")]
@@ -16,18 +21,33 @@ namespace projServer.Controllers
     {
         private readonly IDeviceLogService _logService;
         private readonly IMapper _mapper;
-
+        
+         /// <summary>
+        /// constructor for devicelogcontroller
+        /// injects the log service and automapper.
+        /// </summary>
         public DeviceLogController(IDeviceLogService logService, IMapper mapper)
         {
             _logService = logService;
             _mapper = mapper;
+        
         }
+          /// <summary>
+        /// retrieves all logs for a specific device.
+        /// </summary>
+        /// <param name="deviceId">the unique id of the device</param>
+        /// <returns>list of logs for the given device.</returns>
         [HttpGet("{deviceId}")]
         public async Task<IActionResult> GetLogsByDeviceId(int deviceId)
         {
             var logs = await _logService.GetLogs(deviceId);
             return Ok(logs);
         }
+        
+        /// <summary>
+        /// retrieves all device logs in the system
+        /// </summary>
+        /// <returns>list of all device logs.</returns>
         [HttpGet]
         public async Task<IActionResult> GetAllLogs()
         {
@@ -36,3 +56,4 @@ namespace projServer.Controllers
         }
     }
 }
+
