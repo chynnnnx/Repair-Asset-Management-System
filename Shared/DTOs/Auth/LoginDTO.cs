0f2a91e1439f7cf48dca99fabd56eb5ac13f1c7f
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-09-01 20:14:24 +0800

    Update LoginDTO.cs

--- Shared/DTOs/Auth/LoginDTO.cs
+++ Shared/DTOs/Auth/LoginDTO.cs
@@ -4,7 +4,7 @@
     {
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-        public string Tag { get; set; } = string.Empty;
-        public int RoomId { get; set; }
+      
     }
 }
+
