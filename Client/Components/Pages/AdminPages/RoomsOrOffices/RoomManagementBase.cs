a3aa87dc8decba9a63c5fa2e20a54c935bd52e5c
Cheenee Mae Dimla <cheeneemaedimla@gmail.com>
2025-11-22 18:01:35 +0800

    Refactor RoomManagementBase for improved clarity

--- Client/Components/Pages/AdminPages/RoomsOrOffices/RoomManagementBase.cs
+++ Client/Components/Pages/AdminPages/RoomsOrOffices/RoomManagementBase.cs
@@ -120,10 +120,10 @@ namespace Client.Components.Pages.AdminPages.RoomsOrOffices
             {
                 Position = DialogPosition.TopCenter,
                 CloseOnEscapeKey = true,
-                CloseButton = true
+                CloseButton = false
             };
 
-            var dialog = await DialogService.ShowAsync<ConfirmationDialog>("Confirm Deletion", parameters, options);
+            var dialog = await DialogService.ShowAsync<ConfirmationDialog>("", parameters, options);
             var result = await dialog.Result;
 
             return !result.Canceled;
@@ -165,3 +165,4 @@ namespace Client.Components.Pages.AdminPages.RoomsOrOffices
         }
     }
 }
+
